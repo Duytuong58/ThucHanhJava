affbe114e318a0e30869a416c72ebd651bdb043c
sinhvien <sinhvien>
2021-03-16 16:09:45 +0700

    first commit

---
+++ Tuan1/src/tuan1/Bai3.java
@@ -0,0 +1,51 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tuan1;
+import java.io.*;
+import java.util.*;
+class vd
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner x=new Scanner(System.in);
+        System.out.print("Nhap ma sinh vien:");
+        String masv=x.nextLine();
+        
+        //
+        System.out.print("Ho ten:");
+        String hoten=x.nextLine();
+        //
+        System.out.print("Nhap tuoi:");
+        byte tuoi=x.nextByte();
+        //
+        System.out.print("Nan sinh:");
+        float namsinh=x.nextFloat();
+        //
+        System.out.print("Diem trung binh:");
+        float diemtb=x.nextFloat();
+        //
+    }
+}
+
+/**
+ *
+ * @author Administrator
+ */
+public class Bai3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello! I’m Nguyen Duy Tuong");
+        System.out.println("This is my first java program");
+    }
+    
+}
